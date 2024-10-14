c5eb39d54c6253657db40c42d6bda6a048235a59
Paul-Philippe Nelles <ppnelles@gmail.com>
2024-10-14 15:54:17 +0200

    init

---
+++ themes/stickwithus/template-parts/content-favicons.php
@@ -0,0 +1,6 @@
+<link rel="icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/img/favicons/favicon-48x48.png" sizes="48x48" />
+<link rel="icon" type="image/svg+xml" href="<?php bloginfo('template_directory'); ?>/img/favicons/favicon.svg" />
+<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/img/favicons/favicon.ico" />
+<link rel="apple-touch-icon" sizes="180x180" href="<?php bloginfo('template_directory'); ?>/img/favicons/apple-touch-icon.png" />
+<meta name="apple-mobile-web-app-title" content="Stick With Us" />
+<link rel="manifest" href="<?php bloginfo('template_directory'); ?>/img/favicons/site.webmanifest" />
